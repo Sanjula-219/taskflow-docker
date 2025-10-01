069826b9faa2bdc976740c70dd8ad4eaec006b9c
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-10-01 11:16:57 +0530

    feat: add MongoDB connection configuration

--- app/server.js
+++ app/server.js
@@ -1,1 +1,21 @@
-//mongodb+srv://sanjula229:sanjula123@taskflow-docker.sdif8nt.mongodb.net/
+const mongoose = require("mongoose");
+const express = require("express");
+require("dotenv").config(); // Load .env file
+
+const app = express();
+
+// Middleware
+app.use("/", (req, res, next) => {
+  res.send("Middleware Executed....");
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
